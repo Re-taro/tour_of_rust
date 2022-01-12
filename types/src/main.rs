b1060ca572378932c8a7f277f9639ee86ce2741b
Re-taro <aminokacho@gmail.com>
2022-01-12 18:52:42 +0900

    ✨ understand translate

--- types/src/main.rs
+++ types/src/main.rs
@@ -10,4 +10,10 @@ fn main() {
         "{} {} {} {} {} {} {} {}",
         x, a, b, c, bv, t.0, t.1, sentence
     );
+    let a = 13u8;
+    let b = 7u32;
+    let c = a as u32 + b;
+    println!("{}", c);
+    let t = true;
+    println!("{}", t as u8);
 }
