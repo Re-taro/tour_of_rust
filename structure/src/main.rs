5de6517e3a048bf1900d66b6e9529050b6655445
Re-taro <aminokacho@gmail.com>
2022-01-15 15:38:43 +0900

    ✨ understand creating data inside memory

--- structure/src/main.rs
+++ structure/src/main.rs
@@ -1,4 +1,4 @@
-struct _SeaCreature {
+struct SeaCreature {
     animal_type: String,
     name: String,
     arms: i32,
@@ -9,4 +9,26 @@ struct _SeaCreature {
 fn main() {
     let s = String::from("Hello World!");
     println!("{} is {} char long.", s, s.len());
+    let ferris = SeaCreature {
+        animal_type: String::from("crab"),
+        name: String::from("Ferris"),
+        arms: 2,
+        legs: 4,
+        weapon: String::from("claw"),
+    };
+    let sarah = SeaCreature {
+        animal_type: String::from("octopus"),
+        name: String::from("Sarah"),
+        arms: 8,
+        legs: 0,
+        weapon: String::from("none"),
+    };
+    println!(
+        "{} is a {}. They have {} arms, {} legs, and a {} weapon",
+        ferris.name, ferris.animal_type, ferris.arms, ferris.legs, ferris.weapon
+    );
+    println!(
+        "{} is a {}. They have {} arms, and {} legs. They have no weapon..",
+        sarah.name, sarah.animal_type, sarah.arms, sarah.legs
+    );
 }
